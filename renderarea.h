577bf97887b17a1532099a62f2b74fdba14b97e1
Bălănescu Adrian <balanescuadrian71@gmail.com>
2018-07-14 22:54:39 +0300

    created render area

---
+++ renderarea.h
@@ -0,0 +1,46 @@
+#ifndef RENDERAREA_H
+#define RENDERAREA_H
+
+#include <QWidget>
+
+class RenderArea : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit RenderArea(QWidget *parent = nullptr);
+
+    QSize minimumSizeHint() const Q_DECL_OVERRIDE;
+    QSize sizeHint() const Q_DECL_OVERRIDE;
+
+    enum ShapeType
+    {
+        Astroid, Cycloid, HuygensCycloid, HypoCycloid
+    };
+
+    void setBackgroundColor(QColor color)
+    {
+        mBackgroundColor = color;
+    }
+
+    QColor getBackgroundColor () const
+    {
+        return mBackgroundColor;
+    }
+
+    void setShape (ShapeType shape) {mShape = shape;}
+    ShapeType shape() const {return mShape;}
+
+protected:
+    void paintEvent(QPaintEvent *event) Q_DECL_OVERRIDE;
+
+signals:
+
+public slots:
+
+private:
+    QColor mBackgroundColor;
+    QColor mShapeColor;
+    ShapeType mShape;
+};
+
+#endif // RENDERAREA_H
